d7480143561cecc0e5166ea3c14c7ef86b7095a2
Patrick Pischulti <papi4050@colorado.edu>
2021-11-28 14:15:40 -0700

    Minimal functionality implemented. It can now grab a frame and save it in the Images folder

--- train_face.py
+++ train_face.py
@@ -1,7 +1,33 @@
 import os
+import cv2
+import time
 
-def createImageDir():
-    path = './Images'
+path = './Images'
 
+def createImageDir(path):
     if os.path.isdir(path) is not True:
         os.makedirs(path)
+        print("Dir created!")
+    else:
+        print("Dir already exist")
+
+def captureFace(path):
+    cap = cv2.VideoCapture(0)
+    time.sleep(1)
+    
+
+    success = True
+    count = 0
+    while success:
+        success,image = cap.read()
+        cv2.imwrite(path + "/frame%d.png" % count, image)     # save frame as PNG file   
+        #cv2.imwrite(os.path.join(path , '/frame%d.png'),count, image)   
+        print('Read a new frame: ', success)
+        count += 1
+        if count == 10:
+            success = False
+        time.sleep(1)
+
+createImageDir(path)
+
+captureFace(path)
