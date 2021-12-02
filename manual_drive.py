b28a3a14f07b96ba7c62062165919a67844ff5b2
mihi2266 <mihi2266@colorado.edu>
2021-12-02 23:23:52 +0000

    added key monitor to script

--- manual_drive.py
+++ manual_drive.py
@@ -1,11 +1,44 @@
 import com_module
+from pynput.keyboard import Key, Listener
 
-def drive_controller():
+
+
+'''
+This file contains a function used to transfer keyboard inputs 
+(up down left and right) to the com module to be sent to R2.
+This will be used in case a manual mode is required to control the robot
+
+'''
+
+
+def drive_controller(Key):
+    '''
+    Parameters
+    ----------
+    Key : 
+
+    Returns
+    -------
+    driveValue : int
+        This is the value sent to the com_module for the magnitude of how much
+        R2 will drive forward or backcommand sent to our control module which will tell
+        the robot to go forward or backward
+    turnValue : int
+        This is the value sent to the com_module for the magnitude of how much
+        R2 will drive left or right
+    '''
+    #this listens to see if keys are pressed
+    if Key == keyboard.Key.w:
+        print("forward Pressed")
+    
+    
     # This function only needs to be sent once
     # TODO: Determine proper location for this
     ser = com_module.initSerialConnection("//dev/ttyACM0", 115200)
 
 
+    
+    
     # This is the command to send data via the serial terminal
     # 'ser' needs to set to provide connection info
     # 'data' is the information you are trying to send via serial
