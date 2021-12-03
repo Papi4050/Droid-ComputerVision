ad1821f3d9b213a76b0a841cdf4ed43505d042f7
Patrick Pischulti <papi4050@colorado.edu>
2021-12-03 12:49:17 -0700

    Updated unknownFaceTrack() to receive config file inputs

--- live_tracking.py
+++ live_tracking.py
@@ -165,13 +165,13 @@ def findCenterHaar(imgObjects, objects):
     return cx, cy, imgObjects
 
 
-def unknownFaceTrack():
+def unknownFaceTrack(ser, cascade_path):
 
     #cap = system_setup.configurator()
 
     cap = cv2.VideoCapture(0)
 
-    faceCascade = cv2.CascadeClassifier("../Prototyping/resources/haarcascade_frontalface_default.xml")
+    faceCascade = cv2.CascadeClassifier(cascade_path)
 
     while True:
         success, img = cap.read()
