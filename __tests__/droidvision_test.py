e178ad1c29e64bea878fffc18cf7d3470a19f089
Patrick Pischulti <papi4050@colorado.edu>
2021-12-08 16:11:05 -0700

    Added base template for unit test

--- __tests__/droidvision_test.py
+++ __tests__/droidvision_test.py
@@ -3,10 +3,15 @@ import os, sys
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
+import droidvision
+
 # A class in which we test functions of our module
 class droidvisionTest(unittest.TestCase):
 
     # Test function goes here
+    def test_input_parser(self):
+        # TODO: Fill test 
+        config_list = []
 
 if __name__ == "__main__":
     unittest.main()
