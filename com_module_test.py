64f08571a5978e5b1f7ab928a3248026be038f63
Patrick Pischulti <papi4050@colorado.edu>
2021-12-08 12:36:05 -0700

    Bugfix: renamed function to match required syntax

--- com_module_test.py
+++ com_module_test.py
@@ -4,9 +4,9 @@ import com_module as cm
 # A class in which we test functions of our module
 class ComModuleTest(unittest.TestCase):
 
-    def sendData(self):
-        myString = cm.sendData("", 555, 3)
-        self.assertEqual(myString, '555')
+    def test_sendData_string(self):
+        myString = cm.sendData("", "555", 3)
+        self.assertEqual(myString, '$005005005')
 
 if __name__ == "__main__":
     unittest.main()
