d268dad6fb8a9ecc7ccdad265d3f1f8d151140ca
Michael Hirsch <mihi2266@colorado.edu>
2021-12-07 21:27:36 -0700

    lowered the pd controller K values to 0.1 from 0.5

--- pid_module.py
+++ pid_module.py
@@ -10,8 +10,8 @@
 
 def pidControllerLR(errorLR,oldError):
    #K values for PID tuning (modify these)
-   kLR = [0.5, 0.5]
-   kFB = [0.5, 0.5]
+   kLR = [0.1, 0.1]
+   kFB = [0.1, 0.1]
    #turn function that calculates the turn value
    turn = kLR[0]* errorLR + kLR[1]*(errorLR- oldError)
    return turn
