efa75133bd929617278dba3dfc0887ea1059732a
Patrick Pischulti <papi4050@colorado.edu>
2021-12-08 13:35:47 -0700

    Added unit test file template

---
+++ manual_drive_test.py
@@ -0,0 +1,9 @@
+import unittest
+
+# A class in which we test functions of our module
+class manualDriveTest(unittest.TestCase):
+
+    # Test function goes here
+
+if __name__ == "__main__":
+    unittest.main()
