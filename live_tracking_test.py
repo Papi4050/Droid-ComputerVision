eae853ecf401a31672d79ac0711f64e18e45c231
Patrick Pischulti <papi4050@colorado.edu>
2021-12-08 13:33:47 -0700

    Added unit test file template

---
+++ live_tracking_test.py
@@ -0,0 +1,10 @@
+import unittest
+
+# A class in which we test functions of our module
+class liveTrackingTest(unittest.TestCase):
+
+    # Test function goes here
+
+
+if __name__ == "__main__":
+    unittest.main()
