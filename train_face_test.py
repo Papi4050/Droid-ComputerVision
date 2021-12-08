74cac3b01f2ea6783250256c6a58b65d56a97958
Patrick Pischulti <papi4050@colorado.edu>
2021-12-08 13:37:38 -0700

    Added unit test file template

---
+++ train_face_test.py
@@ -0,0 +1,9 @@
+import unittest
+
+# A class in which we test functions of our module
+class trainFaceTest(unittest.TestCase):
+
+    # Test function goes here
+
+if __name__ == "__main__":
+    unittest.main()
