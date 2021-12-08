399d81653b67676b0272bce0709a28654a57d0ec
Patrick Pischulti <papi4050@colorado.edu>
2021-12-08 12:16:04 -0700

    Updated test file

--- com_module_test.py
+++ com_module_test.py
@@ -5,7 +5,6 @@ import com_module as cm
 class ComModuleTest(unittest.TestCase):
 
     def sendData(self):
-        
         myString = cm.sendData("", 555, 3)
         self.assertEqual(myString, '555')
 
