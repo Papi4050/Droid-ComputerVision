fea44099cc5275b7979fbcf07bc6ddcffea1184e
amta3208 <amta3208@colorado.edu>
2021-12-10 15:48:01 -0700

    Added unit testing for the turn input serial output

--- __tests__/live_tracking_test.py
+++ __tests__/live_tracking_test.py
@@ -5,6 +5,7 @@ import os, sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 import live_tracking
+import serial
 
 # A class in which we test functions of our module
 class liveTrackingTest(unittest.TestCase):
@@ -19,6 +20,17 @@ class liveTrackingTest(unittest.TestCase):
 
         res = live_tracking.estDistance(y1, x2, y2, x1, h, w)
         self.assertAlmostEqual(res, 22.55, 3)
+
+    def test_calculateTurnInput(self):
+        driveConfig = {
+            "left_max":-1,
+            "right_max": 1,
+            "forward_max": 1,
+            "back_max": -1
+        }
+        ser = serial.Serial()
+        res = live_tracking.calculateTurnInput(driveConfig, 1, 1, 1)
+        self.assertAlmostEqual(res, ser.write(333027))
 
 
 if __name__ == "__main__":
