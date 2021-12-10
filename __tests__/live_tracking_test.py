d246ce0ad24b0a0a80300816f3766a8250139d4d
Patrick Pischulti <papi4050@colorado.edu>
2021-12-10 14:38:12 -0700

    Added unit test for estDistance()

--- __tests__/live_tracking_test.py
+++ __tests__/live_tracking_test.py
@@ -9,8 +9,19 @@ import live_tracking
 # A class in which we test functions of our module
 class liveTrackingTest(unittest.TestCase):
 
+    def setUp(self) -> None:
+        return super().setUp()
+
     def test_estDistance(self):
-        temp = []
+        x1 = 10
+        x2 = 210
+        y1 = 10 
+        y2 = 210 
+        h = 1280
+        w = 720
+
+        res = live_tracking.estDistance(y1, x2, y2, x1, h, w)
+        self.assertAlmostEqual(res, 22.55, 3)
 
 
 if __name__ == "__main__":
