6f392368ed676baea97848b6d35add6730952efc
mihi2266 <mihi2266@colorado.edu>
2021-12-11 02:31:10 +0000

    made pep8 compliant

--- __tests__/train_face_test.py
+++ __tests__/train_face_test.py
@@ -1,14 +1,16 @@
 import unittest
-import os, sys
+import os
+import sys
+import train_face
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-import train_face
 
 # A class in which we test functions of our module
 class trainFaceTest(unittest.TestCase):
 
     # Test function goes here
+
 
 if __name__ == "__main__":
     unittest.main()
