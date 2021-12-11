ae1e510aa93c4cfff5702b51f77e0bd6bb93eddc
mihi2266 <mihi2266@colorado.edu>
2021-12-11 02:30:23 +0000

    made pep8 compliant

--- __tests__/system_setup_test.py
+++ __tests__/system_setup_test.py
@@ -1,14 +1,16 @@
 import unittest
-import os, sys
+import os
+import sys
+import system_setup
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-import system_setup
 
 # A class in which we test functions of our module
 class systemSetupTest(unittest.TestCase):
 
     # Test function goes here
+
 
 if __name__ == "__main__":
     unittest.main()
