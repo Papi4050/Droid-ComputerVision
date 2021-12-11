ac6fbad54da845c74beb44124bcefa9edabd5e40
mihi2266 <mihi2266@colorado.edu>
2021-12-11 02:14:37 +0000

    made pep8 compliant

--- __tests__/droidvision_test.py
+++ __tests__/droidvision_test.py
@@ -1,17 +1,19 @@
 import unittest
-import os, sys
+import os
+import sys
+import droidvision
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-import droidvision
 
 # A class in which we test functions of our module
 class droidvisionTest(unittest.TestCase):
 
     # Test function goes here
     def test_input_parser(self):
-        # TODO: Fill test 
+        # TODO: Fill test
         config_list = []
+
 
 if __name__ == "__main__":
     unittest.main()
