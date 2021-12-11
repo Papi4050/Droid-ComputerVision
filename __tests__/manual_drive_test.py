1dfe46811d820d16b9a8b295427c3fccf7aeeb38
mihi2266 <mihi2266@colorado.edu>
2021-12-11 02:29:13 +0000

    made pep8 compliant

--- __tests__/manual_drive_test.py
+++ __tests__/manual_drive_test.py
@@ -1,14 +1,16 @@
 import unittest
-import os, sys
+import os
+import sys
+import manual_drive
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-import manual_drive
 
 # A class in which we test functions of our module
 class manualDriveTest(unittest.TestCase):
 
     # Test function goes here
+
 
 if __name__ == "__main__":
     unittest.main()
