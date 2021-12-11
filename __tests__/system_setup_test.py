3ed126f1940ac592875b05596c092994f1ed8581
amta3208 <amta3208@colorado.edu>
2021-12-10 22:55:12 -0700

    added ststem start up unit testing

--- __tests__/system_setup_test.py
+++ __tests__/system_setup_test.py
@@ -1,5 +1,6 @@
 import unittest
 import os, sys
+import platform
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
@@ -8,7 +9,12 @@ import system_setup
 # A class in which we test functions of our module
 class systemSetupTest(unittest.TestCase):
 
-    # Test function goes here
+    def test_running_on_jetson_nano(self):
+        x86 = 'x86_64'
+        jetson = "aarch64"
+
+        res = system_setup.running_on_jetson_nano()
+        self.assertEquals(res, platform.machine())
 
 if __name__ == "__main__":
     unittest.main()
